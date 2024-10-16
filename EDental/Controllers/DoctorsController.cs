1679202fd4484c90ed3fc00249a6da8ece741c1a
Rakesh Tharu <rakeshdangaura999@gmail.com>
2024-10-16 09:00:51 +0545

    Added Bootstrap Icons Library

--- EDental/Controllers/DoctorsController.cs
+++ EDental/Controllers/DoctorsController.cs
@@ -1,4 +1,5 @@
 using EDental.Data;
+using EDental.Data.Models;
 using Microsoft.AspNetCore.Mvc;
 
 public class DoctorsController: Controller
@@ -8,5 +9,40 @@ public class DoctorsController: Controller
         var db = new EDentalDbContext();
         var doctors = db.Doctors.ToList();
         return View(doctors);
+    }
+
+    [HttpGet]
+    public IActionResult Add()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Add(Doctors doctors)
+    {
+        //Save to db
+        var db = new EDentalDbContext();
+        db.Doctors.Add(doctors);
+        db.SaveChanges();
+        return RedirectToAction(nameof(Index));
+    }
+    [HttpPost]
+    public IActionResult Delete(int id)
+    {
+        Console.Write(id);
+        var db = new EDentalDbContext();
+        var doctor = db.Doctors.Find(id);
+        if(doctor !=null)
+        {
+            db.Doctors.Remove(doctor);
+            db.SaveChanges();
+           
+        }
+        return RedirectToAction(nameof(Index));
+    }
+    [HttpGet]
+    public IActionResult Update()
+    {
+        return View();
     }
 }
